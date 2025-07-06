844b92f88337affbffc42c5679daf3e7916a6691
Antoine Cerdan <eosiswastaken@gmail.com>
2025-07-06 22:18:36 +0200

    add base display functionnality

--- client/src/pages/Display.jsx
+++ client/src/pages/Display.jsx
@@ -1,16 +1,50 @@
-import { useWebSocket } from "../hooks/useWebsocket";
+import React, { useEffect, useState } from 'react';
+import { useDisplayWebSocket } from '../hooks/useWebsocket';
+import styles from './Display.module.css';
+import logo from '../../public/logo.png';
 
-export default function Display() {
-    const { connected, gameState, sendMessage } = useWebSocket('display');
+// Composant Display - Super propre !
+export default function DisplayComponent() {
+    const { isConnected, lastMessage } = useDisplayWebSocket();
+    const [ping, setPing] = useState(false);
+    const [gameState, setGameState] = useState({})
+    const [revealedAnswers, setRevealedAnswers] = useState([]);
+
+    useEffect(() => {
+        if (lastMessage) {
+            console.log('Remote reçu:', lastMessage);
+
+            switch (lastMessage.type) {
+                case 'PING':
+                    setPing(true);
+                    setTimeout(() => setPing(false), 4000);
+                    break;
+                case 'UPDATE_QUESTION':
+                    setGameState(lastMessage.data);
+                    break;
+                case 'REVEAL_ANSWER':
+                    setRevealedAnswers(prev => [...prev, lastMessage.data.answerId]);
+                    break;
+
+
+            }
+        }
+    }, [lastMessage]);
 
     return (
-        <div className="display-container">
-            <div className="connection-status">
-                {connected ? '🟢 WS Connected' : '🔴 WS Disconnected'}
+        <div className={styles.main}>
+            <div className={styles.header}>
+                <img className={styles.logo} src={logo} alt="" />
             </div>
-
-            <h1>Mode Display</h1>
-            {/* Ton interface de display ici */}
+            {ping && <p>Pong !</p>}
+            <p>Status : {isConnected ? 'connected' : 'disconnected'}</p>
+            <p className={styles.title}>Current Question: {gameState.question}</p>
+            <p>Current Answers:</p>
+            {gameState.answers && Object.entries(gameState.answers).map(([key, value]) => (
+                <div key={key}>
+                    <p> Answer {key}: </p><p className={revealedAnswers.includes(value.id) ? styles.shown : styles.hidden}> {value.text} - Votes: {value.votes}</p>
+                </div>
+            ))}
         </div>
     );
 };
