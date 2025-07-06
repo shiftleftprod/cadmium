0473a10b35d30ea4bffd2f5ec40f83eaa6da5ca5
Antoine Cerdan <eosiswastaken@gmail.com>
2025-07-06 13:54:13 +0200

    add base display and remote components

--- client/src/pages/Display.jsx
+++ client/src/pages/Display.jsx
@@ -1,8 +1,16 @@
+import { useWebSocket } from "../hooks/useWebsocket";
+
 export default function Display() {
+    const { connected, gameState, sendMessage } = useWebSocket('display');
+
     return (
-        <div>
-            <h1>Display Page</h1>
-            <p>This is the Display page.</p>
+        <div className="display-container">
+            <div className="connection-status">
+                {connected ? '🟢 WS Connected' : '🔴 WS Disconnected'}
+            </div>
+
+            <h1>Mode Display</h1>
+            {/* Ton interface de display ici */}
         </div>
     );
-}
+};
--- client/src/pages/Remote.jsx
+++ client/src/pages/Remote.jsx
@@ -1,8 +1,17 @@
+import { useWebSocket } from "../hooks/useWebsocket";
+
+
 export default function Remote() {
+    const { connected, gameState, sendMessage } = useWebSocket('remote');
+
     return (
-        <div>
-            <h1>Remote Page</h1>
-            <p>This is the Remote page.</p>
+        <div className="remote-container">
+            <div className="connection-status">
+                {connected ? '🟢 WS Connected' : '🔴 WS Disconnected'}
+            </div>
+
+            <h1>Mode Remote</h1>
+            <button onClick={() => sendMessage({ type: 'PING' })}>next question</button>
         </div>
     );
-}
+};
